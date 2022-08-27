ee4e50108f391385d225279b66adaa01651e3237
brunodono <bruno_bignard@hotmail.com>
2022-08-27 12:23:54 +0100

    Created jsonserver and created a db.json with default values as coming from an API to the events

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -6,20 +6,7 @@ import { IFiltroDeEventos } from "../interfaces/IFiltroDeEventos";
 export const listaDeEventosState = atom<IEvento[]>({
     key: 'listaDeEventosState',
     default: [
-        {
-            "descricao": "Estudar React",
-            "inicio": new Date("2022-08-15T09:00"),
-            "fim": new Date("2022-08-15T13:00"),
-            "completo": false,
-            "id": 1642342747
-        },
-        {
-            "descricao": "Estudar Recoil",
-            "inicio": new Date("2022-08-16T09:00"),
-            "fim": new Date("2022-08-16T11:00"),
-            "completo": false,
-            "id": 1642342959
-        }]
+        ]
 });
 
 export const filtroDeEventos = atom<IFiltroDeEventos>({
