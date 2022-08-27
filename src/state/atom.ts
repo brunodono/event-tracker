e8e1201475aebbec0f46a1469def452b97a70359
brunodono <bruno_bignard@hotmail.com>
2022-08-27 11:58:18 +0100

    Made filter work out and implemented and extracted its logic

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,5 +1,6 @@
 import { atom } from "recoil";
 import { IEvento } from "../interfaces/IEvento";
+import { IFiltroDeEventos } from "../interfaces/IFiltroDeEventos";
 
 
 export const listaDeEventosState = atom<IEvento[]>({
@@ -20,3 +21,8 @@ export const listaDeEventosState = atom<IEvento[]>({
             "id": 1642342959
         }]
 });
+
+export const filtroDeEventos = atom<IFiltroDeEventos>({
+    key:'filtroDeEventos',
+    default: {}
+})
